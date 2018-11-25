4e790532c629a14e9203cd5f8b3d8668fdfa5b3e
Jake Tyler <jrtyler@byu.edu>
2018-11-25 00:58:02 -0700

    scroll test

--- src/components/Professor.vue
+++ src/components/Professor.vue
@@ -1,39 +1,76 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout justify-center align-center>
-      <v-flex xs12 sm8>
-      <v-card>
-
-        <v-card-title primary-title>
-          <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-        </v-card-title>
-
-        <v-card-text>
-          Located two hours south of Sydney in the Southern Highlands of New South Wales, ...
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+  <div>
+    <v-layout column justify-center align-center>
+      <v-subheader>Offset Top</v-subheader>
+      {{ offsetTop }}
     </v-layout>
-  </v-container>
+    <v-container
+      v-on:scroll="onScroll"
+      id="scroll-target"
+      style="max-width: 400px"
+      class="scroll-x"
+    >
+      <v-layout
+        column
+        align-center
+        justify-center
+        style="width: 1000px"
+      >
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
+
+<!--template>
+  <v-container fluid fill-height>
+    <v-layout justify-center align-center>
+
+      <div class="scrolling-wrapper-flexbox">
+          <v-card class="card" sm3>
+            <v-card-title primary-title>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            </v-card-title>
+            <v-card-text>
+              Located two hours south of Sydney in the Southern Highlands of New South Wales, ...
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="orange">More</v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card class="card" sm3>
+            <v-card-title primary-title>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            </v-card-title>
+            <v-card-text>
+              Located two hours south of Sydney in the Southern Highlands of New South Wales, ...
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="orange">More</v-btn>
+            </v-card-actions>
+          </v-card>
+      </div>
+    </v-layout>
+  </v-container>
+</template-->
+
 <script>
+
   export default {
     data: () => ({
-      card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.',
+      offsetTop: 0
     }),
 
-    props: {
-      source: String
+    methods: {
+      onScroll (e) {
+        this.offsetTop = e.target.scrollTop;
+      }
     }
   }
 </script>
 
 <style scope>
-
+card {
+  display: inline;
+}
 </style>
