cdba5e40bc5bdf83b90b39a042fa9c7f73ae4d12
Jake Tyler <jrtyler@byu.edu>
2018-11-25 00:10:25 -0700

    setting up first card

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,62 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Student Opinion</v-toolbar-title>
     </v-toolbar>
 
+
     <v-content>
-      <HelloWorld/>
+      <Professor/>
     </v-content>
+
+
+    <v-footer color="indigo" app>
+      <span class="white--text" id="footer">&copy; 2017</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import Professor from './components/Professor'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+  export default {
+    name: 'App',
+    components: {
+      Professor
+    },
+    data: () => ({
+      drawer: null
+    }),
   }
-}
 </script>
+
+<style scope>
+#footer {
+  text-align: center;
+  width: 100%;
+  margin: auto;
+}
+</style>
---
+++ src/components/Professor.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm8>
+      <v-card>
+
+        <v-card-title primary-title>
+          <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+        </v-card-title>
+
+        <v-card-text>
+          Located two hours south of Sydney in the Southern Highlands of New South Wales, ...
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.',
+    }),
+
+    props: {
+      source: String
+    }
+  }
+</script>
+
+<style scope>
+
+</style>
