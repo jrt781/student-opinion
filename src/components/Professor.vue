6b52f528bb8d923c649ce7b379a6ff36fefbb0ab
Jake Tyler <jrtyler@byu.edu>
2018-11-25 22:52:54 -0700

    changed to use next/prev buttons

--- src/components/Professor.vue
+++ src/components/Professor.vue
@@ -1,76 +1,113 @@
 <template>
-  <div>
-    <v-layout column justify-center align-center>
-      <v-subheader>Offset Top</v-subheader>
-      {{ offsetTop }}
-    </v-layout>
-    <v-container
-      v-on:scroll="onScroll"
-      id="scroll-target"
-      style="max-width: 400px"
-      class="scroll-x"
-    >
-      <v-layout
-        column
-        align-center
-        justify-center
-        style="width: 1000px"
-      >
-      </v-layout>
-    </v-container>
+  <div class="aspect">
+    <v-btn round color="primary" class="change" v-on:click="prev" v-bind:disabled="reviewIndex == 0">
+      <prev-icon />
+    </v-btn>
+
+    <v-flex sm3 class="card-container" v-for="review in reviewsToDisplay" :key="review.id">
+      <v-card class="card" sm3>
+        <v-card-title primary-title>
+          <h3 class="headline mb-0">{{review.course}}, {{review.semester}}</h3>
+        </v-card-title>
+        <v-card-text>
+          {{review.text}}
+        </v-card-text>
+        <v-card-actions>
+          <v-btn flat color="orange">More</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+
+    <v-btn round color="primary" v-on:click="next" :disabled="reviewIndex == reviews.length - 3">
+      <next-icon />
+    </v-btn>
   </div>
 </template>
 
-
-<!--template>
-  <v-container fluid fill-height>
-    <v-layout justify-center align-center>
-
-      <div class="scrolling-wrapper-flexbox">
-          <v-card class="card" sm3>
-            <v-card-title primary-title>
-              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            </v-card-title>
-            <v-card-text>
-              Located two hours south of Sydney in the Southern Highlands of New South Wales, ...
-            </v-card-text>
-            <v-card-actions>
-              <v-btn flat color="orange">More</v-btn>
-            </v-card-actions>
-          </v-card>
-          <v-card class="card" sm3>
-            <v-card-title primary-title>
-              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            </v-card-title>
-            <v-card-text>
-              Located two hours south of Sydney in the Southern Highlands of New South Wales, ...
-            </v-card-text>
-            <v-card-actions>
-              <v-btn flat color="orange">More</v-btn>
-            </v-card-actions>
-          </v-card>
-      </div>
-    </v-layout>
-  </v-container>
-</template-->
-
 <script>
+import NextIcon from "vue-material-design-icons/ChevronRight.vue"
+import PrevIcon from "vue-material-design-icons/ChevronLeft.vue"
 
   export default {
     data: () => ({
-      offsetTop: 0
+      reviewIndex: 0,
+      reviews: [
+        {
+          course: 'CS 236',
+          semester: 'Winter 2018',
+          text: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
+        },
+        {
+          course: 'CS 237',
+          semester: 'Winter 2018',
+          text: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
+        },
+        {
+          course: 'CS 238',
+          semester: 'Winter 2018',
+          text: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
+        },
+        {
+          course: 'CS 239',
+          semester: 'Winter 2018',
+          text: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
+        },
+        {
+          course: 'CS 240',
+          semester: 'Winter 2018',
+          text: 'Located two hours south of Sydney in the Southern Highlands of New South Wales, ...',
+        },
+      ],
     }),
 
-    methods: {
-      onScroll (e) {
-        this.offsetTop = e.target.scrollTop;
+    computed: {
+      reviewsToDisplay: function() {
+        var toDisplay = [];
+        toDisplay.push(this.reviews[this.reviewIndex]);
+        if (this.reviewIndex + 1 < this.reviews.length) {
+          toDisplay.push(this.reviews[this.reviewIndex + 1]);
+        }
+        if (this.reviewIndex + 2 < this.reviews.length) {
+          toDisplay.push(this.reviews[this.reviewIndex + 2]);
+        }
+        return toDisplay;
       }
+    },
+
+    components: {
+      NextIcon,
+      PrevIcon
+    },
+
+    methods: {
+      next: function() {
+        this.reviewIndex += 3;
+        if (this.reviewIndex + 3 >= this.reviews.length) {
+          this.reviewIndex = this.reviews.length - 3;
+        }
+      },
+      prev: function() {
+        this.reviewIndex -= 3;
+        if (this.reviewIndex < 0) {
+          this.reviewIndex = 0;
+        }
+      },
     }
   }
 </script>
 
 <style scope>
-card {
-  display: inline;
+.card-container {
+  margin: 1em;
+  float: left;
+}
+
+.aspect {
+  display : flex;
+  align-items : center;
+}
+
+.change {
+  width: 10px;
 }
 </style>
