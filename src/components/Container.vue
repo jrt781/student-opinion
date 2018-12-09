b4ec3744c2ffaa4b8a75627635ed1768e66140f0
Jake Tyler <jrtyler@byu.edu>
2018-12-09 01:40:44 -0700

    dialog

---
+++ src/components/Container.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="app">
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list dense>
+        <v-list-tile @click="yeet">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="yeet">
+          <v-list-tile-action>
+            <v-icon>search</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Search</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+
+
+
+        <v-list-group prepend-icon="sort">
+          <v-list-tile slot="activator">
+            <v-list-tile-title>Filter</v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-tile id="first-filter">
+            <v-list-tile-content>
+              <v-select
+                v-model="selectedCourses"
+                :items="courses"
+                chips
+                label="Courses to display"
+                multiple
+                outline
+              ></v-select>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-select
+                v-model="selectedSortingMethod"
+                :items="sortingMethods"
+                label="Sort reviews by"
+                outline
+                class="filter"
+              ></v-select>
+            </v-list-tile-content>
+          </v-list-tile>
+
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- TODO: change color of app! -->
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Student Opinion</v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <Professor
+        v-on:report-courses="reportCourses"
+        :selected-courses="selectedCourses"
+        :selected-sorting-method="selectedSortingMethod"
+        />
+    </v-content>
+
+    <!-- TODO: change color of app! -->
+    <v-footer color="indigo" app>
+      <span class="white--text" id="footer">&copy; 2018</span>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+  import Professor from './Professor'
+
+  export default {
+    name: 'Container',
+    components: {
+      Professor
+    },
+    data: () => ({
+      drawer: null,
+      selectedCourses: [],
+      selectedSortingMethod: 'semester, most recent',
+      sortingMethods: [
+        "rating, highest",
+        "rating, lowest",
+        "semester, most recent",
+        "semester, oldest",
+      ],
+      courses: [],
+    }),
+    methods: {
+      yeet: function() {
+      },
+      reportCourses: function(courses) {
+        this.courses = courses;
+        this.selectedCourses = courses;
+      },
+    }
+  }
+</script>
+
+<style scope>
+#footer {
+  text-align: center;
+  width: 100%;
+  margin: auto;
+}
+
+.v-text-field.v-text-field--enclosed .v-text-field__details, .v-text-field.v-text-field--enclosed .v-input__slot {
+  padding: 0 12px 5px;
+}
+
+.v-input__slot {
+  margin-bottom: 0;
+}
+
+.v-list--dense .v-list__tile:not(.v-list__tile--avatar) {
+  min-height:40px;
+  height: auto;
+}
+
+#first-filter {
+  margin-top: 15px;
+}
+</style>
--- src/components/Professor.vue
+++ src/components/Professor.vue
@@ -10,9 +10,47 @@
     <div id="customized-score" class="professor-score">
       <rating v-bind:score="customizedScore" v-bind:size="3"/>
       <div class="score-description">Your customized score</div>
-      <v-btn outline fab small color="indigo" class="edit-custom-score">
+
+
+      <v-dialog
+        v-model="dialog"
+        width="500"
+      >
+      <v-btn
+      slot="activator"
+      outline fab small color="indigo" class="edit-custom-score">
         <v-icon>edit</v-icon>
       </v-btn>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            I accept
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+
     </div>
 
     <br />
@@ -76,6 +114,7 @@
 
     data: function () {
       return {
+        dialog: false,
         name: 'John Smith',
         customizedScore: 7.2,
         department: '',
