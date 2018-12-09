0ef46d2067cae7f69a7efae95f4e29812b06c593
Jake Tyler <jrtyler@byu.edu>
2018-12-09 01:43:28 -0700

    dialog text

--- src/components/Professor.vue
+++ src/components/Professor.vue
@@ -12,44 +12,33 @@
       <div class="score-description">Your customized score</div>
 
 
-      <v-dialog
-        v-model="dialog"
-        width="500"
-      >
-      <v-btn
-      slot="activator"
-      outline fab small color="indigo" class="edit-custom-score">
-        <v-icon>edit</v-icon>
-      </v-btn>
+      <v-dialog v-model="dialog" width="500" >
+        <v-btn slot="activator" outline fab small color="indigo" class="edit-custom-score">
+          <v-icon>edit</v-icon>
+        </v-btn>
 
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Privacy Policy
-        </v-card-title>
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>
+            Customized Score
+          </v-card-title>
 
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
+          <v-card-text>
+            Choose what will be part of calculating your customized score of this professor:
+          </v-card-text>
 
-        <v-divider></v-divider>
+          <v-divider></v-divider>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            flat
-            @click="dialog = false"
-          >
-            I accept
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-
-
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary"@click="dialog = false">
+              Cancel
+            </v-btn>
+            <v-btn color="primary"@click="dialog = false">
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 
     </div>
 
